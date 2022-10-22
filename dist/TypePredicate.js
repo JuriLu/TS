08db896699a0cea0f1c10fd0f35d24de8cc46ab4
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-22 19:43:32 +0200

    fix

--- dist/TypePredicate.js
+++ dist/TypePredicate.js
@@ -2,16 +2,16 @@
 //Type predicates
 function isCat(animal) {
     return animal.numLives !== undefined;
-    // return true or false based on the animal that is turned in has the 
+    // return true or false based on the animal that is turned in has the
     //   property num lives is undefined or not , that is how we check in the function make noise
     // STILL THIS DOESN'T NARROW THE TYYPE, THAT IS WHERE TYPE PREDICATE GOES ON  in the return syntax of the function
 }
 function makeNoise(animal) {
     if (isCat(animal)) {
         animal;
-        return 'meow';
+        return "meow";
     }
     else {
-        animal;
+        return "woof";
     }
 }
