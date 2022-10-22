3c812addb5d93e20e7007101253b05c765489c6c
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-22 20:06:17 +0200

    Lecture: Delayed initialization and Implicit any

--- dist/3.Any Type.js
+++ dist/3.Any Type.js
@@ -7,5 +7,13 @@ thing = "1";
 thing = false;
 thing();
 thing.toUpperCase() `It lets you do all these, but it's not supposed to be like this`;
-let thing2 = 'hello';
+let thing2 = "hello";
 // thing2()
+//Delayed initialization and Implicit any
+const movies = ["Arrival", "The Thing", "Aliens", "Amadeus"];
+// let foundMovies;
+let foundMovies;
+for (let movie of movies) {
+    if (movie === "Amadeus")
+        foundMovies = "Amadeus";
+}
--- src/3.Any Type.ts
+++ src/3.Any Type.ts
@@ -7,9 +7,19 @@ thing = 1;
 thing = "1";
 thing = false;
 thing();
-thing.toUpperCase()
+thing.toUpperCase()`It lets you do all these, but it's not supposed to be like this`;
 
-`It lets you do all these, but it's not supposed to be like this`;
-
-let thing2 = 'hello'
+let thing2 = "hello";
 // thing2()
+
+
+
+//Delayed initialization and Implicit any
+const movies = ["Arrival", "The Thing", "Aliens", "Amadeus"];
+
+// let foundMovies;
+let foundMovies: string;
+
+for (let movie of movies) {
+  if (movie === "Amadeus") foundMovies = "Amadeus";
+}
