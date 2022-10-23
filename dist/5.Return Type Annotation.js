85e924254c0cfaa31bb01abe0f09d28b7de7d68b
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-23 10:50:20 +0200

    Lecture: Return Type annotation

---
+++ dist/5.Return Type Annotation.js
@@ -0,0 +1,10 @@
+"use strict";
+`Also the return type of a function can be annotated`;
+//Function with return type
+const addNums = (x, y) => {
+    return x + y;
+};
+addNums(5, 5); // returns number
+function square(num) {
+    num * num; // returns void
+}
---
+++ src/5.Return Type Annotation.ts
@@ -0,0 +1,15 @@
+`Also the return type of a function can be annotated`;
+
+//Function with return type
+
+const addNums = (x: number, y: number) => {
+  return x + y;
+};
+
+addNums(5,5)   // returns number
+
+
+function square(num:number){
+    num * num  // returns void
+}
+
