b44c15cf331684defe30d2b9c35f4323ca3b44c3
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-24 10:17:01 +0200

    Excessive types

--- src/6.Object Types.ts
+++ src/6.Object Types.ts
@@ -5,7 +5,7 @@
 
 
 
-//* Object type
+//? Object type
 
 function printName( person:{firstName: string, lastName: string} ){
   console.log(`${person.firstName}, ${person.lastName}`);
@@ -21,7 +21,8 @@ printName(
 
 
 
-//* More object type
+
+//? More object type
 
 // let coordinate: {x: number, y: number} = {x: 34}
 let coordinate: {x: number, y: number} = {x: 34,y: 44};
@@ -30,3 +31,16 @@ let coordinate: {x: number, y: number} = {x: 34,y: 44};
 function randomCoordinate(): {x: number, y: number} {
      return {x:2,y:3}
 }
+
+
+//? Excess Properties
+
+// printName({firstName: "Mick", lastName: "Jagger", age: 23}) //! This doesn't work
+
+const singer = {firstName: "Mick", lastName: "Jagger", age: 23}
+printName(singer)                                              //! This works
+
+/* NOTE:
+ * It's because of a decision that TS developers made for this case. When write inline in the first case it will throw an error,
+ * but when passed as a const it will not, but TS will check only for needed variables and will ignore the Excessive Properties
+*/
