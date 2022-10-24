bd22bd49d964501cfd81a44e8f8c6745605c6e2c
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-24 10:53:01 +0200

    Lecture: Readonly modifier

---
+++ dist/9.Readonly Modifies.js
@@ -0,0 +1,6 @@
+"use strict";
+// ! Readonly Modifier
+const user = {
+    id: 1234,
+    username: 'ctts'
+};
---
+++ src/9.Readonly Modifies.ts
@@ -0,0 +1,12 @@
+// ! Readonly Modifier
+
+type User = {
+  readonly id: number;
+  username: string;
+};
+
+const user: User = {
+     id: 1234,
+     username:'ctts'
+};
+
