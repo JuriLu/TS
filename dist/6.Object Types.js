0c9b21ad39110b5f5513ab4c335eb692ce18197b
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-24 10:01:04 +0200

    Lecture: Object Type

---
+++ dist/6.Object Types.js
@@ -0,0 +1,19 @@
+"use strict";
+// * BETTER COMMENTS
+// !
+// ?
+// TODO:            
+//* Object type
+function printName(person) {
+    console.log(`${person.firstName}, ${person.lastName}`);
+}
+printName({
+    firstName: 'asdf',
+    lastName: 'asdf'
+});
+//* More object type
+// let coordinate: {x: number, y: number} = {x: 34}
+let coordinate = { x: 34, y: 44 };
+//* Annotationg the return type with object type
+function randomCoordinate() {
+}
---
+++ src/6.Object Types.ts
@@ -0,0 +1,32 @@
+// * BETTER COMMENTS
+// !
+// ?
+// TODO:            
+
+
+
+//* Object type
+
+function printName( person:{firstName: string, lastName: string} ){
+  console.log(`${person.firstName}, ${person.lastName}`);
+  
+}
+
+printName(
+     {
+          firstName: 'asdf',
+          lastName: 'asdf'
+     }
+)
+
+
+
+//* More object type
+
+// let coordinate: {x: number, y: number} = {x: 34}
+let coordinate: {x: number, y: number} = {x: 34,y: 44};
+
+//* Annotationg the return type with object type
+function randomCoordinate(): {x: number, y: number} {
+
+}
