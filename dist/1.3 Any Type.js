cdd4c55ca584ef73f081d9f0ab25b26bfbc6db4f
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-28 20:52:54 +0200

    Files' name changed

---
+++ dist/1.3 Any Type.js
@@ -0,0 +1,19 @@
+"use strict";
+`'any' is an escape hatch, it turns off type checking`;
+//the any type
+let thing = "hello";
+thing = 1;
+thing = "1";
+thing = false;
+thing();
+thing.toUpperCase() `It lets you do all these, but it's not supposed to be like this`;
+let thing2 = "hello";
+// thing2()
+//Delayed initialization and Implicit any
+const movies = ["Arrival", "The Thing", "Aliens", "Amadeus"];
+// let foundMovies;
+let foundMovies;
+for (let movie of movies) {
+    if (movie === "Amadeus")
+        foundMovies = "Amadeus";
+}
---
+++ dist/2.2 Return Type Annotation.js
@@ -0,0 +1,37 @@
+"use strict";
+`Also the return type of a function can be annotated`;
+// ? Function with return type
+const addNums = (x, y) => {
+    return x + y;
+};
+addNums(5, 5); // returns number
+function cuSquare(num) {
+    num * num; // returns void
+}
+`Anonymous function contextual typing`;
+// * In this case the parameter type are infered
+const colors = ["red", "orange", "yellow"];
+const changed = () => {
+    colors.map((color) => {
+        color.toUpperCase();
+    });
+};
+`Void Return Type`;
+function printTwice(msg) {
+    console.log(msg);
+    console.log(msg);
+    // return `alksdfj`
+}
+` Never Type
+    When it comes to function, the never type annotates a function that should never return
+    A function that never finishes executing
+    A function that throws an exception
+`;
+const neverStop = () => {
+    while (true) {
+        console.log("I am still going");
+    }
+};
+const giveError = (msg) => {
+    throw new Error(msg);
+};
