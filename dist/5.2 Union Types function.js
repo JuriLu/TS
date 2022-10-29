e4b2cfbe15385718f03747a8d118d46a1699a094
Jurgen Lubonja <lubonjajurgen1@gmail.com>
2022-10-29 15:19:19 +0200

    Lecture: Union Types in Function

---
+++ dist/5.2 Union Types function.js
@@ -0,0 +1,21 @@
+"use strict";
+//* Union types with functions
+function printAge(age) {
+    console.log(`You are ${age} yeard old`);
+}
+printAge(23);
+printAge("23");
+//* Must check type , because depending on the type of the parameter we want to make actions
+//* we do this with TYPE NARROWING
+function calculateTax(price, tax) {
+    //  return price * tax   THIS WON'T DO
+    // price.replace("$","")  THIS WON'T DO
+    if (typeof price === "string") {
+        price = parseFloat(price.replace("$", "")); //? Remove $ and convert it to number
+    }
+    else {
+        return price * tax;
+    }
+}
+45.5; //? Price as number
+("$45.50"); //? Price as string, in this case for e.x we want to remove $
